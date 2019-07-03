dc7f4b98b9f438ab79975a083424ddedd62d90b4
aschemily <aschemily@gmail.com>
2019-07-03 10:51:33 -0400

    changing files

--- displaymodal.js
+++
@@ -1,8 +0,0 @@
-var body = document.getElementById("mod-body")
-					var docFrag = document.createDocumentFragment();
-					productImages.forEach(function(url, index, originalArray){
-						varimg = document.createElement('img')
-						img.src = url
-						docFrag.appendChild(img)
-					})
-					body.appendChild(docFrag)
